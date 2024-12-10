ad1acd239da650b562767a53cf1283b5a558d92c
x <29695350+codingbycl@users.noreply.github.com>
2024-12-10 08:45:53 -0600

    fix

--- src/libs/indexer/utils/create-turso.ts
+++ src/libs/indexer/utils/create-turso.ts
@@ -2,8 +2,7 @@ import { createClient } from "@libsql/client";
 
 export function createTurso() {
   const client = createClient({
-    url: "file:./db.db",
-    syncUrl: process.env.TURSO_DATABASE_URL,
+    url: process.env.TURSO_DATABASE_URL,
     authToken: process.env.TURSO_AUTH_TOKEN,
   });
 
