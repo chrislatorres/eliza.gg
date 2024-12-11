fff3d241671fff8e83f4c21692007cf5c278ba9a
x <29695350+codingbycl@users.noreply.github.com>
2024-12-11 02:11:54 -0600

    fix

--- src/app/(app)/search/page.tsx
+++ src/app/(app)/search/page.tsx
@@ -1,7 +1,12 @@
 import { Chat } from "@/components/app/chat";
+import { Suspense } from "react";
 
 export const experimental_ppr = true;
 
 export default function Page() {
-  return <Chat />;
+  return (
+    <Suspense fallback={null}>
+      <Chat />
+    </Suspense>
+  );
 }
